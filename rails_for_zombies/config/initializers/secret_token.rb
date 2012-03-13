aaedb3fd515d0b7738a6a474312aee56d068baef
unknown <alumnos@.upc.edu.pe>
2012-03-12 19:25:16 -0500

    msj

---
+++ rails_for_zombies/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsForZombies::Application.config.secret_token = 'b9ab549774b1de046fa8c9e0b53b144c68d2b128772823b4b1596ccf52b56e9b5d2f7a94f46b3c52d639a6c808b1379657a46ec266612cb19c0c41098d531057'
